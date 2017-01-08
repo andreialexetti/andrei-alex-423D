b56674b08f058e987b164109525a75c87f82e48b
andreialexetti <alexandrescuandrei71@yahoo.com>
2017-01-08 16:20:50 +0200

    Update E1.Exercitiu_t1_Alexandrescu_Andrei.m

--- E1.Exercitiu_t1_Alexandrescu_Andrei.m
+++ E1.Exercitiu_t1_Alexandrescu_Andrei.m
@@ -1,6 +1,8 @@
-%a)La înmul?irea a dou? matrice X ?i Y trebuie ca num?rul de coloane ale matricei X s?
+%a)La Ã®nmul?irea a dou? matrice X ?i Y trebuie ca num?rul de coloane ale matricei X s?
 %fie egal cu num?rul de linii ale matricei Y.
-%Ini?ializare în MATLAB : 
+%Ini?ializare Ã®n MATLAB : 
 a=[0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2]
 b=[1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1]
 a*b
+%Rezolvarea a fost realizata inainte de deadline si incarcata pe github atat pe sectiunea cod cat si in issues . 
+%Prima tema cu deadline 06.11.2016 in sectiunea cod nu poate fi accesata ,iar din acest motiv am mai incarcat din nou prima tema.
